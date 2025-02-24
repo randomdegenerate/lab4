6ccc7ccf951313d46cbdc7e3a2776f0d43f95b23
randomdegenerate <sandilkosgahakumbura@gmail.com>
2025-02-24 13:45:44 +0000

    msg

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  presidents: string[] = ["Higgins", "McAleese", "Robinson", "Hillery"];
+  presidents: string[] = ["Higgins", "McAleese", "Robinson", "Hillery"];//Presidents Array is used in list.component.html
+  presidents2: any[] =
+[{name: "Higgins", term:"2011-"},
+{name: "McAleese", term:"1997-2011"},
+{name: "Robinson", term:"1990-1997"},
+{name: "Hillery", term:"1976-1990"},
+{name: "O Dalaigh", term:"1974-1976"}]
 }
